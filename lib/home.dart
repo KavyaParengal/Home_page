fc9cdf08d56cad3b172ea19cfc65e9ae0c3eb3f6
KavyaParengal <kavyaparengal9037@gmail.com>
2024-01-18 12:12:10 +0530

    Second Commit

--- lib/home.dart
+++ lib/home.dart
@@ -25,20 +25,20 @@ class _Home_pageState extends State<Home_page> {
               padding: const EdgeInsets.only(top: 41,right: 25,left: 25),
               child: Row(
                 children: [
-                  CircleAvatar(
+                  const CircleAvatar(
                     radius: 25,
                     backgroundColor: Colors.lightBlueAccent,
                     child: Text('D',style: TextStyle(fontSize: 28,fontWeight: FontWeight.bold,color: Colors.white),),
                   ),
-                  SizedBox(width: 20,),
-                  Column(
+                  const SizedBox(width: 20,),
+                  const Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text('Duy Truong Haang,',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.white),),
                       Text('Welcome back !',style: TextStyle(fontSize: 16,color: Colors.white),)
                     ],
                   ),
-                  SizedBox(width: 20,),
+                  const SizedBox(width: 20,),
                   Image.asset('images/sparkles.png',
                     width: 40,
                       height: 40,
@@ -46,11 +46,11 @@ class _Home_pageState extends State<Home_page> {
                 ],
               ),
             ),
-            SizedBox(height: 29,),
+            const SizedBox(height: 29,),
             Expanded(
               child: Container(
                 width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
+                decoration: const BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.only(topLeft: Radius.circular(33),topRight: Radius.circular(33))
                 ),
@@ -61,11 +61,11 @@ class _Home_pageState extends State<Home_page> {
                       Row(
                         children: [
                           Icon(Icons.home,color: Colors.deepPurple[600],size: 36,),
-                          SizedBox(width: 13,),
+                          const SizedBox(width: 13,),
                           Text('Home Page',style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500,color: Colors.deepPurple[600]),)
                         ],
                       ),
-                      SizedBox(height: 13,),
+                      const SizedBox(height: 13,),
                       Container(
                         width: MediaQuery.of(context).size.width,
                         height: MediaQuery.of(context).size.height*.16,
@@ -81,14 +81,14 @@ class _Home_pageState extends State<Home_page> {
                                 width: 100,
                                 height: 100,
                               ),
-                              SizedBox(width: 8,),
+                              const SizedBox(width: 8,),
                               Column(
                                 mainAxisAlignment: MainAxisAlignment.center,
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
                                   Text('Play Game',style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold,color: Colors.yellow[700])),
-                                  SizedBox(height: 3,),
-                                  Text('Create room and join \nroom',style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500,color: Colors.white))
+                                  const SizedBox(height: 3,),
+                                  const Text('Create room and join \nroom',style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500,color: Colors.white))
                                 ],
                               )
                             ],
@@ -102,9 +102,9 @@ class _Home_pageState extends State<Home_page> {
                             Expanded(
                                 child: Divider(color: Colors.deepPurple[300],thickness: 2,)
                             ),
-                            SizedBox(width: 17,),
+                            const SizedBox(width: 17,),
                             Text('Some other features',style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500,color: Colors.deepPurple[300])),
-                            SizedBox(width: 17,),
+                            const SizedBox(width: 17,),
                             Expanded(
                                 child: Divider(color: Colors.deepPurple[300],thickness: 2,)
                             ),
@@ -129,9 +129,9 @@ class _Home_pageState extends State<Home_page> {
                                   children: [
                                     Image.asset('images/guide.png',width: 60,height: 60,),
                                     //SizedBox(height: 3,),
-                                    Text('Guide',style: TextStyle(fontSize: 23,color: Colors.white,fontWeight: FontWeight.bold),),
+                                    const Text('Guide',style: TextStyle(fontSize: 23,color: Colors.white,fontWeight: FontWeight.bold),),
                                     //SizedBox(height: 5,),
-                                    Text('Rule and how to \nplay',style: TextStyle(fontSize: 12,color: Colors.white),)
+                                    const Text('Rule and how to \nplay',style: TextStyle(fontSize: 12,color: Colors.white),)
                                   ],
                                 ),
                               ),
@@ -148,9 +148,9 @@ class _Home_pageState extends State<Home_page> {
                                   children: [
                                     Image.asset('images/profile.png',width: 60,height: 60,),
                                     //SizedBox(height: 3,),
-                                    Text('Profile',style: TextStyle(fontSize: 23,color: Colors.white,fontWeight: FontWeight.bold),),
+                                    const Text('Profile',style: TextStyle(fontSize: 23,color: Colors.white,fontWeight: FontWeight.bold),),
                                     //SizedBox(height: 5,),
-                                    Text('Your information \nand history',style: TextStyle(fontSize: 12,color: Colors.white),)
+                                    const Text('Your information \nand history',style: TextStyle(fontSize: 12,color: Colors.white),)
                                   ],
                                 ),
                               ),
@@ -167,9 +167,9 @@ class _Home_pageState extends State<Home_page> {
                                   children: [
                                     Image.asset('images/settings.png',width: 60,height: 60,),
                                     //SizedBox(height: 3,),
-                                    Text('Settings',style: TextStyle(fontSize: 23,color: Colors.white,fontWeight: FontWeight.bold),),
+                                    const Text('Settings',style: TextStyle(fontSize: 23,color: Colors.white,fontWeight: FontWeight.bold),),
                                     //SizedBox(height: 5,),
-                                    Text('Change settings for \ngame',style: TextStyle(fontSize: 12,color: Colors.white),)
+                                    const Text('Change settings for \ngame',style: TextStyle(fontSize: 12,color: Colors.white),)
                                   ],
                                 ),
                               ),
@@ -186,9 +186,9 @@ class _Home_pageState extends State<Home_page> {
                                   children: [
                                     Image.asset('images/about.png',width: 60,height: 60,),
                                     //SizedBox(height: 3,),
-                                    Text('About us',style: TextStyle(fontSize: 23,color: Colors.white,fontWeight: FontWeight.bold),),
+                                    const Text('About us',style: TextStyle(fontSize: 23,color: Colors.white,fontWeight: FontWeight.bold),),
                                     //SizedBox(height: 5,),
-                                    Text('Info aboubt us and \nfeedback',style: TextStyle(fontSize: 12,color: Colors.white),)
+                                    const Text('Info aboubt us and \nfeedback',style: TextStyle(fontSize: 12,color: Colors.white),)
                                   ],
                                 ),
                               ),
